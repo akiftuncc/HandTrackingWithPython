f1184d7af91541707cef27d200c2080ee1840270
akiftuncc <86754778+akiftuncc@users.noreply.github.com>
2022-04-06 20:35:47 +0300

    new
    new

---
+++ HandTrackingProject/Hand_distance.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+liste = []
+while True:
+    succes, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        a = results.multi_hand_landmarks
+        liste.append(a[0].landmark)
+        q = a[0].landmark
+        print(q)
+
+    cTime = time.time()
+    fps = round(1/(cTime-pTime),5)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,5,255),
+                2)
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
